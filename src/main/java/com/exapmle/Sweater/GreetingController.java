ff85339a8c03a9cfa0d506938f98c0bd233b758c
user <sfrvvv@yandex.ru>
2022-02-07 14:51:13 +0300

    user test commit

--- src/main/java/com/exapmle/Sweater/GreetingController.java
+++ src/main/java/com/exapmle/Sweater/GreetingController.java
@@ -4,7 +4,6 @@ import com.exapmle.Sweater.domain.Message;
 import com.exapmle.Sweater.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
